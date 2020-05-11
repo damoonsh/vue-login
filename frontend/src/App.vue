0f825208a7ccd73fcbe5fa1235b11242fc9bcd40
Damoon <d.shahhosseini@yahoo.com>
2020-05-11 02:08:28 -0400

    Refactoring

---
+++ frontend/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-card color="grey lighten-4" flat height="50px" tile>
+      <v-toolbar flat>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Title</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-card>
+
+    <v-content>
+      <Messages />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Messages from "./components/Messages";
+
+export default {
+  name: "App",
+
+  components: {
+    Messages
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ frontend/src/components/Messages.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-list subheader>
+      <v-subheader class="diaplay-1">Messages</v-subheader>
+
+      <v-list-item v-for="message in messages" :key="message">
+        <v-list-item-content>
+          <v-list-item-title v-text="message"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      messages: ["Mamad", "nubari"]
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
