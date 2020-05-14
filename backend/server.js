1e3e105b746cbf727d82350fd23c24e88f498457
Damoon <d.shahhosseini@yahoo.com>
2020-05-14 02:18:34 -0400

    Added the code for JWT

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+
 const app = express();
 const port = 3001;
 
 app.use(bodyParser());
 app.use(cors());
 
-var messages = ["Mamad", "nubari", "sup?"];
+var messages = [{text:"Mamad", user:"Jim"}, {user:"Jim", text:"nubari"}];
+var users = [{username: "Jim", password: "1"}];
 
 app.get('/messages', (req, res) => {
 
@@ -19,12 +22,26 @@ app.get('/messages/:uid', (req, res) => {
 });
 
 app.post('/messages', (req, res) => {
-    let msg = req.body
-    console.log(msg);
+    const token = (req.header('Authorization'));
+    const userId = jwt.decode(token, '123');
+    const user = users[userId]
+    let msg = {
+        text: req.body.message,
+        user: user.userName
+    };
     
-    messages.push(msg.message);
+    messages.push(msg);
     res.json(msg);
-    console.log(messages);
 })
 
-app.listen(port, () => console.log('app running'));
+app.post('/Register', (req, res) => {
+    let registerData = req.body
+    let newIndex = users.push(registerData);
+    let userId = newIndex - 1;
+
+    let token = jwt.sign(userId, '123');
+
+    res.json(token);
+})
+
+app.listen(port, () => console.log('app running on ' + port));
